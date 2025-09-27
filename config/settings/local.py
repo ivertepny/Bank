4b3a9b993c96539987af1bce8c32eecee8443d29
Igor Vertepny <ivertepny@gmail.com>
2025-09-27 12:42:19 +0300

    correct some issues

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,43 +1,38 @@
 from os import getenv, path
-
-from django.conf.global_settings import EMAIL_BACKEND, CSRF_TRUSTED_ORIGINS
 from dotenv import load_dotenv
-from .base import *
+from .base import *  # noqa
 from .base import BASE_DIR
 
 
-local_env_file = path.join(BASE_DIR, '.envs', '.env.local')
-if path.exists(local_env_file):
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.isfile(local_env_file):
     load_dotenv(local_env_file)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
-    #'django-insecure-(%*9tjza&d(6@$o-q3#+_3n4sodzfq0c-p9@3v$c+#)y)=s^*3'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv('DEBUG')
+DEBUG = getenv("DEBUG")
 
-SITE_NAME = getenv('SITE_NAME')
+SITE_NAME = getenv("SITE_NAME")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
-ADMIN_URL = getenv('ADMIN_URL')
+ADMIN_URL = getenv("ADMIN_URL")
 
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
-EMAIL_HOST = getenv('EMAIL_HOST')
-EMAIL_PORT = getenv('EMAIL_PORT')
-DEFAULT_FROM_EMAIL = getenv('DEFAULT_FROM_EMAIL')
-DOMAIN = getenv('DOMAIN')
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
+ADMIN_EMAIL = getenv("ADMIN_EMAIL")
 
-MAXIMUM_UPLOAD_SIZE = 1 * 1024 * 1024  # 1 MB
+MAX_UPLOAD_SIZE = 1 * 1024 * 1024
 
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
 
-LOCKOUT_DURATION =  timedelta(minutes=1)
-LOGGING_ATTEMPTS = 3
-OTP_EXPIRATION = timedelta(minutes=5)
+LOCKOUT_DURATION = timedelta(minutes=1)
+
+LOGIN_ATTEMPTS = 3
+
+OTP_EXPIRATION = timedelta(minutes=1)
